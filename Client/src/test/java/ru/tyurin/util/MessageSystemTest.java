72a87c904ec911553c89b79e908ee19c74340844
tyurin <tyurin23@gmail.com>
2013-07-11 15:20:25 +0400

    add Message System Test

---
+++ Client/src/test/java/ru/tyurin/util/MessageSystemTest.java
@@ -0,0 +1,47 @@
+package ru.tyurin.util;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * User: tyurin
+ * Date: 7/11/13
+ * Time: 1:10 PM
+ */
+public class MessageSystemTest {
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+	@Test
+	public void testAddEvent() throws Exception {
+
+	}
+
+	@Test
+	public void testSendToListeners() throws Exception {
+
+	}
+
+	@Test
+	public void testAddListener() throws Exception {
+
+	}
+
+	@Test
+	public void testGetInstance() throws Exception {
+
+	}
+
+	@Test
+	public void testRun() throws Exception {
+
+	}
+}
