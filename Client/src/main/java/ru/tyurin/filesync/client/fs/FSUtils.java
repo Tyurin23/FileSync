d7549217e43d213ab3473c37a3626136feae6174
tyurin <tyurin23@gmail.com>
2013-07-31 16:22:38 +0400

    fix compare and add block methods

--- Client/src/main/java/ru/tyurin/filesync/client/fs/FSUtils.java
+++ Client/src/main/java/ru/tyurin/filesync/client/fs/FSUtils.java
@@ -4,30 +4,25 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.CRC32;
 import java.util.zip.CheckedInputStream;
 
-/**
- * Created with IntelliJ IDEA.
- * User: tyurin
- * Date: 21.07.13
- * Time: 21:02
- * To change this template use File | Settings | File Templates.
- */
 public class FSUtils {
 
-	public static boolean compare(Path base, FileNode node){
-		if(base == null || node == null){
+	public static boolean compare(Path base, FileNode node) {
+		if (base == null || node == null) {
 			return false;
 		}
 		boolean equals = true;
-		equals = equals && base.equals(node.getPath());
+		equals = equals && base.toAbsolutePath().toString().equals(node.getPath());
 		equals = equals && getSpace(base) == node.getSpace();
 		equals = equals && getHash(base) == node.getHash();
 		return equals;
 	}
 
-	public static long getSpace(Path file){
+	public static long getSpace(Path file) {
 		try {
 			return Files.size(file);
 		} catch (IOException e) {
@@ -36,7 +31,7 @@ public class FSUtils {
 		}
 	}
 
-	public static long getHash(Path file){
+	public static long getHash(Path file) {
 		InputStream istream = null;
 		try {
 			istream = Files.newInputStream(file);
@@ -47,5 +42,18 @@ public class FSUtils {
 			e.printStackTrace();
 			return -1;
 		}
+	}
+
+	public static FileNode createFileNode(Path path) {
+		return new FileNode(path, getSpace(path), getHash(path), getBlocks(path));
+	}
+
+	public static List<FileBlock> getBlocks(Path file) {
+		List<FileBlock> blocks = new ArrayList<>();
+		return blocks;
+	}
+
+	protected static FileBlock getBlock(Path file, int index) {
+		return null;
 	}
 }
