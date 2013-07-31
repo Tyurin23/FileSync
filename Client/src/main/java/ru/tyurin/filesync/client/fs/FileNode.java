c2e07bf057429abf3cb06f73277bd11eb1ce427e
tyurin <tyurin23@gmail.com>
2013-07-31 16:26:39 +0400

    add block and status fields

--- Client/src/main/java/ru/tyurin/filesync/client/fs/FileNode.java
+++ Client/src/main/java/ru/tyurin/filesync/client/fs/FileNode.java
@@ -2,42 +2,54 @@ package ru.tyurin.filesync.client.fs;
 
 import java.io.Serializable;
 import java.nio.file.Path;
+import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: tyurin
- * Date: 7/3/13
- * Time: 11:32 AM
- * To change this template use File | Settings | File Templates.
- */
+
 public class FileNode implements Serializable {
 
 	private String path;
 	private long space;
 	private long hash;
+	private FileStatus status = FileStatus.NEW;
+	private List<FileBlock> blocks;
 
-	public FileNode(Path path) {
-		this(path, -1, -1);
-	}
 
-	public FileNode(Path path, long space, long hash){
+	public FileNode(Path path, long space, long hash, List<FileBlock> blocks) {
 		this.path = path.toString();
 		this.space = space;
 		this.hash = hash;
-
-	}
-
-	public String getPath() {
-		return path;
+		this.blocks = blocks;
 	}
 
 	public long getSpace() {
 		return space;
 	}
 
+	public void setSpace(long space) {
+		this.space = space;
+	}
+
 	public long getHash() {
 		return hash;
 	}
 
+	public void setHash(long hash) {
+		this.hash = hash;
+	}
 
+	public List<FileBlock> getBlocks() {
+		return blocks;
+	}
+
+	public void setBlocks(List<FileBlock> blocks) {
+		this.blocks = blocks;
+	}
+
+	public FileStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(FileStatus status) {
+		this.status = status;
+	}
 }
